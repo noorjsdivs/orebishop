f2309ac0c852a452a09052dad493a2305a3ff84d
Noor Mohammad <noormohammad@AAOMAN.COM>
2022-12-18 16:40:41 +0400

    navbar error running

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { logo } from "../../assets/images";
 import Image from "../designLayouts/Image";
@@ -8,22 +8,42 @@ import List from "../designLayouts/List";
 import ListItem from "../designLayouts/ListItem";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(true);
+
+  useEffect(() => {
+    let ResponsiveMenu = (e) => {
+      if (window.innerWidth < 769) {
+        setShowMenu(false);
+      } else {
+        setShowMenu(true);
+      }
+    };
+    // ResponsiveMenu();
+    window.addEventListener("resize", ResponsiveMenu);
+  });
   return (
-    <nav className="h-20">
+    <nav className="h-20 relative">
       <Flex>
         <div>
           <Image className="w-16 object-cover" imgSrc={logo} />
         </div>
         <div>
-          <HiMenuAlt2 className="bg-red-400 flex mdl:hidden w-8 h-6" />
+          <HiMenuAlt2
+            onClick={() => setShowMenu(!showMenu)}
+            className="bg-red-400 flex mdl:hidden cursor-pointer w-8 h-6 absolute top-6 right-4"
+          />
           <List className="flex gap-1">
-            {navBarList.map(({ _id, title }) => (
-              <ListItem
-                className="hidden mdl:flex font-normal hover:font-bold w-20 h-6 justify-center items-center px-12  text-base text-[#767676] hover:underline underline-offset-[4px] decoration-[1px] hover:text-[#262626] border-r-[2px] border-r-gray-300 hover:border-r-[#262626] hoverEffect last:border-r-0"
-                key={_id}
-                itemName={title}
-              />
-            ))}
+            {showMenu && (
+              <>
+                {navBarList.map(({ _id, title }) => (
+                  <ListItem
+                    className="flex font-normal hover:font-bold w-20 h-6 justify-center items-center px-12  text-base text-[#767676] hover:underline underline-offset-[4px] decoration-[1px] hover:text-[#262626] border-r-[2px] border-r-gray-300 hover:border-r-[#262626] hoverEffect last:border-r-0"
+                    key={_id}
+                    itemName={title}
+                  />
+                ))}
+              </>
+            )}
           </List>
         </div>
       </Flex>
